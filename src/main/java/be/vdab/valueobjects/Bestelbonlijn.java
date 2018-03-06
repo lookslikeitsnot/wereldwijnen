aafddb8d60f58de00b71cb83c26c66907728e4f2
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-06 15:39:54 +0100

    met images

--- src/main/java/be/vdab/valueobjects/Bestelbonlijn.java
+++ src/main/java/be/vdab/valueobjects/Bestelbonlijn.java
@@ -1,12 +1,22 @@
 package be.vdab.valueobjects;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import be.vdab.entities.Wijn;
 
 @Embeddable
 public class Bestelbonlijn implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "wijnid")
+	private Wijn wijn;
 	private int aantal;
+	private BigDecimal prijs;
 }
